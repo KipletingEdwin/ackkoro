7787e5095de46f02f97a65722ce25f44b0bdf0b0
Kipleting <kipletingedwin4@gmail.com>
2022-07-10 12:36:58 +0300

    make first button interactive

---
+++ Assets/index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded',() =>{
+    let myFirstButton = document.getElementById('btn')
+    myFirstButton.addEventListener('click',clickAlert)
+    function clickAlert(){
+         alert('Hello Edwin,Hope your enjoyed Service')
+    }
+})
